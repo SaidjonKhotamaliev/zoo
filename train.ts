2101cca7368dba95607c365e2182531477b83f21
SaidjonKhotamaliev <saidjon.khotamaliev@gmail.com>
2024-08-14 09:51:55 +0500

    feat: train.ts was added to the zoo project

---
+++ train.ts
@@ -0,0 +1,12 @@
+// ZI-TASK:
+
+// Shunday function yozing, u function ishga tushgandan 3 soniyadan keyin "Hello World" ni qaytarsin.
+// MASALAN: delayHelloWorld("Hello World") return "Hello World"
+
+function delayHelloWorld(str: string): void {
+  setTimeout(() => {
+    console.log(str);
+  }, 3000);
+}
+
+console.log(delayHelloWorld('Hello World'));
